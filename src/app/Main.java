83966af6224660e894ed32d42c0498ea27e62cd0
Frolov1987 <116515964+Frolov1987@users.noreply.github.com>
2023-06-06 22:09:11 +0300

    Change app description

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("My git app.");
+        System.out.println("Real application for Users.");
     }
 }
 
