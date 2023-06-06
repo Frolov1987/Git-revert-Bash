0154d5efa56b30b4290f6a5df36dd8e43a48e700
Frolov1987 <116515964+Frolov1987@users.noreply.github.com>
2023-06-06 22:13:10 +0300

    Revert "Change app title"
    This reverts commit 54f6cd212b5ba1a13300631f93cecc2f38664c8b.

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,8 +3,7 @@ package app;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Demo app.");
-        System.out.println("Simple demo App for Users.");
+        System.out.println("My git app.");
     }
 }
 
