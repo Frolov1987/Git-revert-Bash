b0a5138701b6e54fff1ad744e44658039036f17f
Frolov1987 <116515964+Frolov1987@users.noreply.github.com>
2023-06-06 22:06:39 +0300

    Revert: Add app version

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,8 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("My git app.");
-        System.out.println("Simple demo App for Users.");
-        System.out.println("Version: 1.0");
     }
 }
 
