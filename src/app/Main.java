54f6cd212b5ba1a13300631f93cecc2f38664c8b
Frolov1987 <116515964+Frolov1987@users.noreply.github.com>
2023-06-06 22:12:38 +0300

    Change app title

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,7 +3,8 @@ package app;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("My git app.");
+        System.out.println("Demo app.");
+        System.out.println("Simple demo App for Users.");
     }
 }
 
